70a7f5c2bab330cafda8c3128fc214d6fc3acd23
mooree5917 <90165352+mooree5917@users.noreply.github.com>
2021-09-19 23:59:43 -0400

    Add files via upload

---
+++ exercise10/src/main/java/solution10/application.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Eric Moore
+ */
+package solution10;
+
+import java.util.Scanner;
+
+public class application {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the price of the first item: ");
+        int price1 = in.nextInt();
+        System.out.print("Enter the quantity of the item: ");
+        int quantity1 = in.nextInt();
+        System.out.print("Enter the price of the second item: ");
+        int price2 = in.nextInt();
+        System.out.print("Enter the quantity of the item: ");
+        int quantity2 = in.nextInt();
+        System.out.print("Enter the price of the third item: ");
+        int price3 = in.nextInt();
+        System.out.print("Enter the quantity of the item: ");
+        int quantity3 = in.nextInt();
+
+        
+    }
+}
